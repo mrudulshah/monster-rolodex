53ab8064cf8b8310192c735bbcfab7a09a3a1475
Mrudul Shah <mrudul.ce@gmail.com>
2020-04-06 00:04:06 +0530

    Updates

---
+++ precache-manifest.1e2a6b8dad1336ef39d135c3e71549cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3938754451799fad05addc8a9b2b07b5",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "e12b25ce77ee15197904",
+    "url": "/react-rolodex/static/css/main.fedbcab2.chunk.css"
+  },
+  {
+    "revision": "e4f27bc14fb32aa63130",
+    "url": "/react-rolodex/static/js/2.7f209ed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.7f209ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12b25ce77ee15197904",
+    "url": "/react-rolodex/static/js/main.1165f716.chunk.js"
+  },
+  {
+    "revision": "6754a5faa74f8686cdf6",
+    "url": "/react-rolodex/static/js/runtime-main.3d58db75.js"
+  }
+]);
